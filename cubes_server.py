e5c255a79e6c22a5e2adbfc7760f15e1e6839af4
Christoph Broschinski <broschinski@uni-bielefeld.de>
2015-11-25 10:27:16 +0100

    initial commit

---
+++ cubes_server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from sqlalchemy import create_engine
+from cubes.server import slicer
+from flask.ext.cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(slicer, config="slicer.ini")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
---
+++ init_db.py
@@ -0,0 +1,109 @@
+import csv, codecs, sqlalchemy
+
+# These two classes were adopted from 
+# https://docs.python.org/2/library/csv.html#examples
+# UnicodeReader was slightly modified to return a DictReader
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+def create_table_from_csv(connectable, file_name, table_name, fields,
+                          create_id=False, schema=None):
+    """Create a table with name `table_name` from a CSV file `file_name` with columns corresponding
+    to `fields`. The `fields` is a list of two string tuples: (name, type) where type might be:
+    ``integer``, ``float`` or ``string``.
+
+    If `create_id` is ``True`` then a column with name ``id`` is created and will contain generated
+    sequential record id.
+
+    This is just small utility function for sandbox, play-around and testing purposes. It is not
+    recommended to be used for serious CSV-to-table loadings. For more advanced CSV loadings use another
+    framework, such as Brewery (http://databrewery.org).
+    """
+
+    metadata = sqlalchemy.MetaData(bind=connectable)
+
+    table = sqlalchemy.Table(table_name, metadata, autoload=False, schema=schema)
+    if table.exists():
+        table.drop(checkfirst=False)
+
+    type_map = {"integer": sqlalchemy.Integer,
+                "float": sqlalchemy.Numeric,
+                "string": sqlalchemy.String(256),
+                "text": sqlalchemy.Text,
+                "date": sqlalchemy.Text,
+                "boolean": sqlalchemy.Integer}
+
+    if create_id:
+        col = sqlalchemy.schema.Column('id', sqlalchemy.Integer, primary_key=True)
+        table.append_column(col)
+
+    field_names = []
+    for (field_name, field_type) in fields:
+        col = sqlalchemy.schema.Column(field_name, type_map[field_type.lower()])
+        table.append_column(col)
+        field_names.append(field_name)
+
+    table.create()
+
+    reader = UnicodeReader(open(file_name, 'rb'))
+
+    # Skip header
+    next(reader)
+
+    insert_command = table.insert()
+
+    for row in reader:
+        record = dict(zip(field_names, row))
+        insert_command.execute(record)
+
+engine = sqlalchemy.create_engine('sqlite:////var/local/openapc_cubes/cubes.sqlite')
+create_table_from_csv(engine,
+    "apc_de.csv",
+    table_name="openapc",
+    fields=[
+        ("Institution", "string"),
+        ("Period", "string"),
+        ("Euro", "float"),
+        ("publisher", "string"),
+        ("journal_full_title", "string"),
+        ("issn", "string"),
+        ("issn_print", "string"),
+        ("issn_electronic", "string"),
+        ("doi", "string"),
+        ("indexed_in_CrossRef", "string"),
+        ("pmid", "string"),
+        ("pmcid", "string"),
+        ("base_url", "string"),
+        ("repo_id", "string"),
+        ("ut", "string"),
+        ("DOAJ", "string"),
+        ("license_ref", "string"),
+        ("is_hybrid", "string")],
+    create_id=True
+)
